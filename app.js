b7e2539f12fab2e13bfad8a38c1ccd711283da87
msnghgrv <115294538+msnghgrv@users.noreply.github.com>
2024-01-15 17:39:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,29 +7,32 @@ const rateLimiter= require('express-rate-limit')
 
 const express= require('express')
 const app =express()
-const notFoundMiddleware= require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+
+const connectDB= require('./db/connect')
+const authenticateUser= require('./middleware/authentication')
 const authRouter= require('./routes/auth')
 const jobRouter= require('./routes/jobs')
-const authenticateUser= require('./middleware/authentication')
-const connectDB= require('./db/connect')
+
+const notFoundMiddleware= require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 //routers
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', authenticateUser,jobRouter)
-//error handler
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
 
 app.set('trust proxy',1)
 app.use(rateLimiter({
 	windowMs: 15 * 60 * 1000,  
 	limit: 100, 
-	standardHeaders: 'draft-7', 
-	legacyHeaders: false, 
 }))
 app.use(express.json())
 app.use(cors())
 app.use(xss())
+app.use(helmet())
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser,jobRouter)
+//error handler
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware) 
+
+
 
 //connect db
 app.get('/', (req,res)=>{
@@ -37,7 +40,7 @@ app.get('/', (req,res)=>{
 })
 
 
-const port= process.env.PORT || 1000 
+const port= 1000|| process.env.PORT
 const start= async()=>{
   try {
     await connectDB(process.env.MONGO_URI)
